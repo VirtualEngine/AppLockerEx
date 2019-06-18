8b4eac1f2ccdc1563fb3cb8c701f06caf80e3bc9
Iain <iainbrighton@hotmail.com>
2019-06-18 20:17:16 +0100

    Removes AppLocker module dependency

--- AppLockerEx.psm1
+++ AppLockerEx.psm1
@@ -1,5 +1,20 @@
-## Import the \Src files. This permits loading of the module's functions for unit testing, without having to unload/load the module.
 $moduleRoot = Split-Path -Path $MyInvocation.MyCommand.Path -Parent;
+
+#region LocalizedData
+$culture = 'en-US'
+if (Test-Path -Path (Join-Path -Path $moduleRoot -ChildPath $PSUICulture)) {
+    $culture = $PSUICulture
+}
+$importLocalizedDataParams = @{
+    BindingVariable = 'localized';
+    Filename = 'AppLockerEx.Resources.psd1';
+    BaseDirectory = $moduleRoot;
+    UICulture = $culture;
+}
+Import-LocalizedData @importLocalizedDataParams;
+#endregion LocalizedData
+
+## Import the \Src files. This permits loading of the module's functions for unit testing, without having to unload/load the module.
 $moduleSrcPath = Join-Path -Path $moduleRoot -ChildPath 'Src';
 Get-ChildItem -Path $moduleSrcPath -Include *.ps1 -Exclude '*.Tests.ps1' -Recurse |
     ForEach-Object {
@@ -10,5 +25,4 @@ Get-ChildItem -Path $moduleSrcPath -Include *.ps1 -Exclude '*.Tests.ps1' -Recurs
             ));
     }
 
-$xmlExPath = Join-Path -Path $moduleRoot -ChildPath 'Lib\XmlEx'
-Import-Module -Name $xmlExPath -Force;
+Import-Module -Name (Join-Path -Path $moduleRoot -ChildPath 'Lib\XmlEx') -Force;
